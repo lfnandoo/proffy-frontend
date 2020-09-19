3accd6433a34ed21a6b6c368ce4820f4eca8dbdd
Luiz Fernando <devfernandoof@gmail.com>
2020-09-19 01:02:27 -0300

    🚀 page list header

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import PageHeader from "../../components/PageHeader";
+
 export default function TeacherForm() {
-  return <div>Teacher Form</div>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
+  );
 }
