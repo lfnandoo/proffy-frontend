04f2dd5fba6fd7109ef073a1869cc7056b49e25b
Luiz Fernando <devfernandoof@gmail.com>
2020-09-19 22:47:46 -0300

    🚀 teacher item

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,6 +3,8 @@ import "./styles.css";
 
 import PageHeader from "../../components/PageHeader";
 
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
 export default function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
@@ -22,6 +24,42 @@ export default function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars3.githubusercontent.com/u/64756114?s=460&u=9a6e9ca86ae006756b38815a79ed5a23b08eb168&v=4"
+              alt="Luiz Fernando"
+            />
+            <div>
+              <strong>Luiz Fernando</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis,
+            voluptatibus consequatur. Facere officia recusandae similique sit
+            ducimus dolorum atque, veniam quaerat repellat, nesciunt error
+            explicabo doloribus animi amet, qui illo.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
