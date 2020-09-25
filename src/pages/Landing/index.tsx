3db8640b82ca98d94e885d75b21fdeda2667e92e
Luiz Fernando <devfernandoof@gmail.com>
2020-09-25 11:18:39 -0300

    🚀 total connections get api data

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import api from '../../services/api'
 
 import "./styles.css";
 
@@ -10,6 +11,16 @@ import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
 export default function Landing() {
+  const [totalConnections, setTotalConnections] = React.useState(0);
+
+  React.useEffect(() => {
+    api.get('connections').then(res => {
+      const { total } = res.data
+
+      setTotalConnections(total);
+    })
+  }, [])
+
   return (
     <div id="page-landing">
       <div className="container" id="page-landing-content">
@@ -36,7 +47,7 @@ export default function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões realizadas.{" "}
+          Total de {totalConnections} conexões realizadas.{" "}
           <img src={purpleHeartIcon} alt="Coração Roxo" />
         </span>
       </div>
